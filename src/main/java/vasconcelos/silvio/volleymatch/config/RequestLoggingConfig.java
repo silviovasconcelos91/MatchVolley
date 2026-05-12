9570b41e3490450f82fd67a7f1137c8f4443ccd9
Silvio <silviovasconcelos91@gmail.com>
2026-05-12 12:25:48 +0200

    fix(match): add log for debugging

---
+++ src/main/java/vasconcelos/silvio/volleymatch/config/RequestLoggingConfig.java
@@ -0,0 +1,19 @@
+package vasconcelos.silvio.volleymatch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10_000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
